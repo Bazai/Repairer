8865271556d71c1fcda9a2caa4743d4db6463d4a
BazZy <bazzy.bazzy@gmail.com>
2011-09-23 21:49:05 +0400

    js добавление и удаление вложенных моделей по Райну Бейтсу, которое не работает

---
+++ app/assets/javascripts/brands.js
@@ -0,0 +1,12 @@
+// Функция для удаления полей Брэндов
+function remove_fields(link) {
+	$(link).prev("input[type=hidden]").val("1");
+	$(link).closest(".field").hide();
+}
+
+// Функция для добавления новых полей Брэндов
+function add_fields(link, association, content) {
+  var new_id = new Date().getTime();
+  var regexp = new RegExp("new_" + association, "g")
+  $(link).parent().before(content.replace(regexp, new_id));
+}
