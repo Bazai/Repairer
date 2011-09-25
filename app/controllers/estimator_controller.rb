f4a11d97cb026a597ff0163b3cd6269f8ae3186e
BazZy <bazzy.bazzy@gmail.com>
2011-09-25 23:48:00 +0400

    Логика заполнения селектов перенесена в контроллер. Легкий рефакторинг ветвления

--- app/controllers/estimator_controller.rb
+++ app/controllers/estimator_controller.rb
@@ -1,6 +1,31 @@
+# encoding: utf-8
 class EstimatorController < ApplicationController
   def index
     @brands = Brand.all
     @car_models = CarModel.all
+
+    if (params[:brand].blank?)
+      @brands_select_pars = [:brand, :id, @brands, :id, :name, :prompt => "Выбирай брэнд"]
+
+      if params[:car_model].blank?
+        @car_models_select_pars = [:car_model, :id, @car_models, :id, :name, { :prompt => "Модель автомобиля" }, \
+                                   { :disabled => "disabled" }]
+        @production_years_select_pars = [:production_year, :id, @car_models, :id, :name, { :prompt => "Год выпуска" }, \
+                                         { :disabled => "disabled" }]
+      end
+    else
+      @brands_select_pars = [:brand, :id, @brands, :id, :name, { :selected => params[:brand][:id] } ]
+      if params[:car_model].blank?
+        @car_models_select_pars = [:car_model, :id, Brand.find(params[:brand][:id]).car_models, :id, :name, \
+                                   { :prompt => "А вот сейчас выбирай модель" } ]
+        @production_years_select_pars = [:production_year, :id, @car_models, :id, :name, { :prompt => "Год выпуска" }, \
+                                         { :disabled => "disabled" } ]
+      else
+        @car_models_select_pars = [:car_model, :id, Brand.find(params[:brand][:id]).car_models, :id, :name, \
+                                   { :selected => params[:car_model][:id] } ] unless params[:car_model][:id].empty?
+        @production_years_select_pars = [:production_year, :id, CarModel.find(params[:car_model][:id]).production_years, :id, :year, \
+                                         { :prompt => "Можно выбрать год выпуска" } ] unless params[:car_model][:id].empty?
+      end
+    end
   end
 end
