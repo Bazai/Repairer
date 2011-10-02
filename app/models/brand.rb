8cc655e8254d1c35eaee323518d93b2e1f834b52
BazZy <bazzy.bazzy@gmail.com>
2011-10-02 14:45:50 +0400

    Валидация car_model на предмет уникальности имени в рамках родительского брэнда

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -4,4 +4,14 @@ class Brand < ActiveRecord::Base
   validates_uniqueness_of :name, :on => :create, :message => "Название должно быть уникальным"
   # accepts_nested_attributes_for :car_models
   accepts_nested_attributes_for :car_models, :allow_destroy => true, :reject_if => proc { |a| a[:name].blank? }
+  
+  # Возвращает true или false, в зависимости от того, есть ли у данного брэнда модель
+  # с названием car_model_name
+  def has_car_model?(car_model_name)
+    arr = []
+    self.car_models.each do |car_model|
+      arr << car_model.name
+    end
+    arr.include?(car_model_name)
+  end
 end
--- app/models/car_model.rb
+++ app/models/car_model.rb
@@ -4,6 +4,23 @@ class CarModel < ActiveRecord::Base
   belongs_to :brand
   
   validates_presence_of :brand, :on => :create, :message => "наличие брэнда обязательно"
+  validate :validate_name_uniqueness_inside_brand
   
   accepts_nested_attributes_for :production_years, :allow_destroy => true, :reject_if => proc { |a| a[:year].blank? }
+  
+  def validate_name_uniqueness_inside_brand
+    self.errors.add(:name, "модели должно быть уникально, в рамках родительского брэнда") if self.brand.has_car_model?(self.name)
+  end
+  
+  def before_update
+    # puts self.parent.inspect
+    self.errors.add(:name, "модели должно быть уникально, в рамках родительского брэнда") if self.brand.has_car_model?(self.name)
+    return false if self.errors.count > 0
+  end
+
+  def before_create
+    self.errors.add(:name, "модели должно быть уникально, в рамках родительского брэнда") if self.brand.has_car_model?(self.name)
+    return false if self.errors.count > 0
+  end
+  
 end
