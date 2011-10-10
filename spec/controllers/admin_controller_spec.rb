5e2be92ba444c2be5f956f8f5eea17698f1a1ae3
BazZy <bazzy.bazzy@gmail.com>
2011-10-10 16:06:46 +0400

    шаг к тестированию

--- spec/controllers/admin_controller_spec.rb
+++ spec/controllers/admin_controller_spec.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 require 'spec_helper'
 
 describe AdminController do
@@ -7,6 +8,21 @@ describe AdminController do
       get 'index'
       response.should be_success
     end
+  end
+  
+  describe "#parse_maintenance_csv при обработке файла honda_civic_2000.csv " do
+    it "должен показать сообщение об неправильном брэнде в строке 10 " do
+      # controller.stub!(:params).and_return({:file => {:tempfile => Rails.root + "/spec/files/honda_civic_2000.csv" }}) [:file].tempfile = Rails.root + "/spec/files/honda_civic_2000.csv"
+      file_path = Rails.root.join("/spec/files/honda_civic_2000.csv")
+      puts file_path.inspect
+      mocked_file = { :tempfile => file_path }
+      controller.stub!(:params).and_return(:file => mocked_file)
+      post :parse_maintenance_csv
+      file = controller.params[:file]
+      puts file.inspect
+      puts controller.error_array
+    end
+    
   end
 
 end
